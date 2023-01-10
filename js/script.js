9483e944e3dcb8907160089caddafb888998b8ee
rica213 <25f5k252l@mozmail.com>
2023-01-10 20:30:33 +0300

    Add a function to load more guests

--- js/script.js
+++ js/script.js
@@ -4,42 +4,42 @@ const guests = [
     speakerName: 'Random name one',
     title: 'Random title',
     experience: 'Random things done by this person one',
-    headshot: 'https://github.com/rica213/anime-conference/blob/development/img/Makima_headshot.jpg'
+    headshot: 'img/Makima_headshot.jpg'
   },
   {
     id: '2',
     speakerName: 'Random name two',
     title: 'Random title',
     experience: 'Random things done by this person two',
-    headshot: '../img/Makima_headshot.jpg'
+    headshot: 'img/Makima_headshot.jpg'
   },
   {
     id: '3',
     speakerName: 'Random name three',
     title: 'Random title',
     experience: 'Random things done by this person three',
-    headshot: '../img/Makima_headshot.jpg'
+    headshot: 'img/Makima_headshot.jpg'
   },
   {
     id: '4',
     speakerName: 'Random name four',
     title: 'Random title',
     experience: 'Random things done by this person four',
-    headshot: '../img/Makima_headshot.jpg'
+    headshot: 'img/Makima_headshot.jpg'
   },
   {
     id: '5',
     speakerName: 'Random name five',
     title: 'Random title',
     experience: 'Random things done by this person five',
-    headshot: '../img/Makima_headshot.jpg'
+    headshot: 'img/Makima_headshot.jpg'
   },
   {
     id: '6',
     speakerName: 'Random name six',
     title: 'Random title',
     experience: 'Random things done by this person six',
-    headshot: '../img/Makima_headshot.jpg'
+    headshot: 'img/Makima_headshot.jpg'
   },
 ];
 
@@ -47,13 +47,19 @@ const loadGuests = () => {
   const ul = document.querySelector('#guest ul');
   guests.forEach((guest) => {
     const li = document.createElement('li');
-    li.classList.add('guest', 'd-flex', 'col-md-5', 'mt-3');
+    li.classList.add('guest', 'col-md-5', 'mt-3','d-none', 'd-md-flex');
+    if(guest.id === '1' || guest.id ==='2'){
+      li.classList.add('d-flex');
+    } else {
+      li.classList.add('d-none', 'd-md-flex');
+    }
     ul.appendChild(li);
     const headshotWrapper = document.createElement('div');
     headshotWrapper.classList.add('headshot');
     li.appendChild(headshotWrapper);
     const headshotContent = document.createElement('img');
     headshotContent.src = guest.headshot;
+    headshotWrapper.appendChild(headshotContent);
     const resume = document.createElement('div');
     resume.classList.add('resume', 'ms-3', 'd-flex', 'flex-column');
     li.appendChild(resume);
